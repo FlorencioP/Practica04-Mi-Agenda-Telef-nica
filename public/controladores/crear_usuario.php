84a0122fd524b1b195f8e084b21d4967e46e31ad
Charly1590 <carlos19.pe@gmail.com>
2020-05-23 21:46:00 -0500

    validaciones

--- public/controladores/crear_usuario.php
+++ public/controladores/crear_usuario.php
@@ -10,7 +10,7 @@
     </style>
 </head>
     <body>
-        <?php
+    <?php
             include '../../config/conexionBD.php';
             $cedula = isset($_POST["cedula"]) ? trim($_POST["cedula"]) : null;
             $nombres = isset($_POST["nombres"]) ? mb_strtoupper(trim($_POST["nombres"]), 'UTF-8') : null;
@@ -22,6 +22,29 @@
             $correo = isset($_POST["correo"]) ? trim($_POST["correo"]): null;
             $fechaNacimiento = isset($_POST["fechaNacimiento"]) ? trim($_POST["fechaNacimiento"]): null;
             $contrasena = isset($_POST["contrasena"]) ? trim($_POST["contrasena"]) : null;
+            $sql = "INSERT INTO USUARIOS VALUES (0, '$cedula', '$nombres', '$apellidos', '$direccion',
+            '$correo', MD5('$contrasena'), '$fechaNacimiento', 'N', null, null,'U')";
+            if ($conn->query($sql) === TRUE) {
+                $idTel = "SELECT MAX(usu_id) AS usu_id FROM USUARIOS";
+                $result = $conn->query($idTel);
+                while($row=$result->fetch_assoc()){
+                    $id=$row['usu_id'];
+                }
+                $sqlTel = "INSERT INTO telefonos VALUES (0, '$telefono', '$tipo', '$operadora', 'N',
+                NULL,NULL,$id)";
+                if ($conn->query($sqlTel) != TRUE) {
+                    echo "<p class='error'>Error: " . mysqli_error($conn) . "</p>";
+                } 
+            } 
+            else {
+                if($conn->errno == 1062){
+                    echo "<p class='error'>La persona con la cedula $cedula ya esta registrada en el sistema </p>";
+                }else{
+                    echo "<p class='error'>Error: " . mysqli_error($conn) . "</p>";
+                }
+            }
+            $conn->close();
+            echo "<a href='../vista/crear_usuario.html'>Regresar</a>";
         ?>
     </body>
  </html>
