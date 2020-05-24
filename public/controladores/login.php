527bfb32cc261446900690917128e9ec90399942
FlorencioP <sebasale10@hotmail.com>
2020-05-23 21:34:58 -0500

    Avance en el login

--- public/controladores/login.php
+++ public/controladores/login.php
@@ -1,17 +1,21 @@
-<?php
- session_start();
- include '../../config/conexionBD.php';
- $usuario = isset($_POST["correo"]) ? trim($_POST["correo"]) : null;
- $contrasena = isset($_POST["contrasena"]) ? trim($_POST["contrasena"]) : null;
- $sql = "SELECT * FROM usuario WHERE usu_correo = '$usuario' and usu_password =
-MD5('$contrasena')";
- $result = $conn->query($sql);
- if ($result->num_rows > 0) {
- $_SESSION['isLogged'] = TRUE;
- header("Location: ../../admin/vista/usuario/index.php");
- } else {
- header("Location: ../vista/login.html");
- }
+<?php 
+    session_start(); 
+    
+    include '../../config/conexionBD.php'; 
+    
+    $usuario = isset($_POST["correo"]) ? trim($_POST["correo"]) : null; 
+    $contrasena = isset($_POST["contrasena"]) ? trim($_POST["contrasena"]) : null; 
+    $sql = "SELECT * FROM usuarios WHERE usu_correo = '$usuario' and usu_contrasena = '$contrasena'      --MD5('$contrasena')"; 
+    $result = $conn->query($sql); 
+    
+    if ($result->num_rows > 0) { 
+        $_SESSION['isLogged'] = TRUE; 
+        header("Location: ../../admin/vista/usuario/index.php"); 
+    } else { 
+        header("Location: ../vista/paginashtml/login.html"); 
+    } 
+    
+    
+    $conn->close(); 
 
- $conn->close();
 ?>
