10db452e85cdfe82c851dcb570b7b84fbaf9ded8
Charly1590 <carlos19.pe@gmail.com>
2020-05-24 21:42:01 -0500

    activacion de inputs y botones

--- admin/controladores/user/funcionesBtn.js
+++ admin/controladores/user/funcionesBtn.js
@@ -1,4 +1,10 @@
 function modificar(){
-    console.log("si entra");
-    document.getElementById("aceptar").setAttribute("style" ,"display: block;");
+    document.getElementById("aceptar").setAttribute("style" ,"display: inline-block;");
+    document.getElementById("cancelar").setAttribute("style" ,"display: inline-block;");
+    document.getElementById("Cedula").disabled=false;
+    document.getElementById("Nombre").disabled=false;
+    document.getElementById("Apellido").disabled=false;
+    document.getElementById("Direccion").disabled=false;
+    document.getElementById("Fecha").disabled=false;
+    document.getElementById("Correo").disabled=false;
 }
