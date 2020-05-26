b23fec65b33eb9ed3d6c36ed6576204372429ce4
Charly1590 <carlos19.pe@gmail.com>
2020-05-26 18:07:43 -0500

    comit

--- admin/vista/usuario/index.php
+++ admin/vista/usuario/index.php
@@ -27,62 +27,6 @@
             while($row = $result->fetch_assoc()) {
     ?> 
         <div id="centro"> 
-<<<<<<< HEAD
-            <form id="formulario01" method="POST" action="../../controladores/user/modificar.php?id=<?php echo $id ?>" onsubmit="return validarForm2()">
-            <div id="texto">
-                    <div class="inputP">
-                        <label id="text">Cedula:</label>
-                        <br>
-                        <input type="text" id="Cedula" size="30"  name="cedula" value="<?php echo $row["usu_cedula"]; ?>" onkeyup="validarTamano()" onblur="validarCedula()" disabled>
-                        <br>
-                        <span id="msjCedula" class="error"></span>
-                    </div>
-                    
-                    <div class="inputP">
-                        <label id="text">Nombres:</label>
-                        <input type="text" id="Nombre" size="30" name="nombres" value="<?php echo $row["usu_nombres"]; ?>" onblur="validarNombres()" onkeyup="valLeNom()" disabled>
-                        <br>
-                        <span id="msjNombres" class="error"></span>
-                    </div>
-                
-                    <div class="inputP">
-                        <label id="text">Apellido:</label>
-                        <input type="text" id="Apellido" size="30" name="apellidos" value="<?php echo $row["usu_apellidos"]; ?>" onblur="validarApellidos()" onkeyup="valApeLe()" disabled>
-                        <br>
-                        <span id="msjApellido" class="error"></span>
-                    </div>
-                    
-                    <div class="inputP">
-                        <label id="text">Direccion:</label>
-                        <input type="text" id="Direccion" name="direccion" size="30" value="<?php echo $row["usu_direccion"]; ?>" onblur="validarDireccion()" disabled>
-                        <br>
-                        <span id="msjDireccion" class="error"></span>
-                    </div> 
-                    
-                    <div class="inputP">
-                        <label id="text">Fecha de nacimiento:</label>
-                        <input type="date" id="Fecha" name="fechaNacimiento" value="<?php echo $row["usu_fecha_nacimiento"]; ?>"  onkeyup="validarFecha()" placeholder="Ingrese su
-                        fecha de nacimiento ..." disabled>
-                        <br>
-                        <span id="msjFecha" class="error"></span>
-                    </div>
-                    
-                    <div class="inputP">
-                        <label id="text">Correo:</label>
-                        <input type="text" id="Correo" size="30" name="correo" value="<?php echo $row["usu_correo"]; ?>" onblur="validarCorreo()" disabled>
-                        <br>
-                        <span id="msjCorreo" class="error"></span>
-                    </div>
-
-                    <div id="btnlog">
-                        <input style="display: none;" class="btnLog" type="submit" value="Aceptar" name="aceptar" id="aceptar" onclick="validarCorreo();validarFecha();validarDireccion();validarApellidos();
-                        validarNombres();validarTamano();">
-                        <input style="display: none;" type="reset" id="cancelar" name="cancelar" value="Cancelar" onclick="bloqueo();"/>
-                    </div>
-                </div>
-            </form> 
-        </div> 
-=======
         <form id="formulario01" method="POST" action="../../controladores/crear_usuario.php" onsubmit="return validarForm()">
         <div id="texto">
                 <div class="inputP">
@@ -128,7 +72,6 @@
                     <br>
                     <span id="msjCorreo" class="error"></span>
                 </div>
->>>>>>> 54a28e9e2d08571685108f8a084143a7a5cc9e90
 
         <div id="centro2" style="display: none">
             <form id="formulario01" method="POST" action="../../controladores/user/cambiar_contrasena.php?id=<?php echo $id ?>" onsubmit="return validarForm3()">
