60259f82580ebe69faa01946feb32d36b5d135ae
FlorencioP <sebasale10@hotmail.com>
2020-05-27 15:40:06 -0500

    Añadido el Boton buscar para los telefonos

---
+++ admin/controladores/user/numsUser.js
@@ -0,0 +1,54 @@
+
+
+function mostrarNumero( id){
+
+    if (window.XMLHttpRequest) { 
+        // code for IE7+, Firefox, Chrome, Opera, Safari 
+        xmlhttp = new XMLHttpRequest(); 
+    } else { // code for IE6, IE5 
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP"); 
+    } 
+
+    xmlhttp.onreadystatechange = function() { 
+        if (this.readyState == 4 && this.status == 200) { 
+            //alert("llegue"); 
+            document.getElementById("tabla").innerHTML = this.responseText; 
+        } 
+    }; 
+    
+    xmlhttp.open("GET","../../controladores/user/numeros.php?id="+id,true); 
+    xmlhttp.send(); 
+
+}
+
+function buscarNumero( id ){
+
+    var num = document.getElementById("numero").value; 
+    console.log(num)
+    console.log(id)
+    if (num == "") { 
+
+        document.getElementById("tabla").innerHTML = " <b>No se a detectado el Numero </b>"; 
+
+
+    } else { 
+        if (window.XMLHttpRequest) { 
+            // code for IE7+, Firefox, Chrome, Opera, Safari 
+            xmlhttp = new XMLHttpRequest(); 
+        } else { // code for IE6, IE5 
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP"); 
+        } 
+
+        xmlhttp.onreadystatechange = function() { 
+            if (this.readyState == 4 && this.status == 200) { 
+                //alert("llegue"); 
+                document.getElementById("tabla").innerHTML = this.responseText; 
+            } 
+        }; 
+        
+        xmlhttp.open("GET","../../controladores/user/numerosBud.php?id="+id+" &num="+num,true); 
+        xmlhttp.send(); 
+    } 
+    return false;
+
+}
--- admin/vista/usuario/index.php
+++ admin/vista/usuario/index.php
@@ -3,6 +3,7 @@
     if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] === FALSE){ 
         header("Location: /SistemaDeGestion/public/vista/login.html"); 
     } 
+    $id=$_GET['id'];
 ?>
 
 <!DOCTYPE html> 
@@ -15,12 +16,13 @@
     <link   rel="stylesheet" type="text/css" href="../../../public/vista/css/estilosUser.css"/>
     <script type="text/javascript" src="../../../public/controladores/validaciones.js"></script>
     <script type="text/javascript" src="../../controladores/user/funcionesBtn.js"></script>
+    <script src='../../controladores/user/numsUser.js' ></script>
 </head> 
 
-<body> 
+<body onload="mostrarNumero(<?php echo $id ?>);"> 
     <?php 
         include '../../../config/conexionBD.php'; 
-        $id=$_GET['id'];
+        
         $sql = "SELECT * FROM usuarios where usu_id=$id"; 
         $result = $conn->query($sql); 
         if ($result->num_rows > 0) { 
@@ -151,55 +153,36 @@
         }
         } else { 
             echo "<tr>"; 
-            echo " <td colspan='7'> No existen usuarios registradas en el sistema </td>"; 
+            echo " <td colspan='7'> No existen usuarios registrados en el sistema </td>"; 
             echo "</tr>"; 
         } 
         $conn->close(); 
     ?>
-    <div id="tabla" >
-        <style type="text/css">
-            .tg  {border-collapse:collapse;border-spacing:0;}
-            .tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
-            overflow:hidden;padding:10px 5px;word-break:normal;}
-            .tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
-            font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
-            .tg .tg-46ru{background-color:#96fffb;border-color:inherit;text-align:left;vertical-align:top}
-            .tg .tg-y698{background-color:#efefef;border-color:inherit;text-align:left;vertical-align:top}
-            .tg .tg-y6988{background-color:rgb(240, 248, 255,0.6);border-color:inherit;text-align:left;vertical-align:top}
-        </style>
-        <table class="tg" style="width:100%">
-            <thead>
-            <tr>
-                <th class="tg-46ru">Numero</th>
-                <th class="tg-46ru">Tipo</th>
-                <th class="tg-46ru">Operadora</th>
-                <th class="tg-46ru"></th>
-                <th class="tg-46ru"></th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php 
-                include '../../../config/conexionBD.php'; 
-                $id=$_GET['id'];
-                $sql = "SELECT * FROM telefonos where USUARIOS_usu_id=$id and 	tel_eliminado='N' "; 
-                $result = $conn->query($sql); 
-                if ($result->num_rows > 0) {
-                    while($row = $result->fetch_assoc()) {
-            ?> 
-                <tr>
-                    <td class="tg-y698"><?php echo $row["tel_numero"] ?></td>
-                    <td class="tg-y698"><?php echo $row["tel_tipo"] ?></td>
-                    <td class="tg-y698"><?php echo $row["tel_operadora"] ?></td>
-                    <td class="tg-y698"><span style="cursor: pointer" onclick="cambiarRenglon(this,<?php echo $row['tel_id'] ?>,<?php echo $id ?>)">Modificar</span></td>
-                    <td class="tg-y698"><a href='../../controladores/user/elimTelefono.php?idT=<?php echo $row["tel_id"] ?>+&id=<?php echo $id ?>'>Eliminar</a></td>
-                </tr> 
-            <?php 
-               }
-            }
-            $conn->close(); 
-            ?> 
-            </tbody>
-        </table>
+
+
+<form id="BusquedaTelf">
+
+        <label for="numero">Busqueda de Numero: </label> <br>
+
+        <input type="text" id="numero" name="numero" value="" placeholder="Ingrese el numero a buscar"/> <br>
+
+        <input type="button" id="numBusc" name="numBusc" value="Buscar numero" onclick="buscarNumero(<?php echo $id ?>)" /><br>  
+        
+        
+
+ </form>
+
+
+ <input type="button" id="numMos" name="numMos" value="Mostrar Numeros" onclick="mostrarNumero(<?php echo $id ?>);" /><br>  
+
+
+   
+   <div id="tabla" >
+
     </div>
+
+
+
+
 </body> 
 </html>
